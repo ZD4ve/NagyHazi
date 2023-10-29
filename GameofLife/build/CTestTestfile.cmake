2be4208b704c2f96965abfcd9d01a9825f8e4e77
ZD4ve <zoller.davidkiraly@gmail.com>
2023-10-29 20:27:47 +0100

    qcc start2

--- GameofLife/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/FILES/Egyetem/1_szemeszter/PROGalap1/NagyHazi/GameofLife
-# Build directory: /mnt/c/FILES/Egyetem/1_szemeszter/PROGalap1/NagyHazi/GameofLife/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
