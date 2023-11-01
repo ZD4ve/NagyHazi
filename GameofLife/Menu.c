70b461a2963d4de0e4e9681d8ab4d54b83d721f2
ZD4ve <zoller.davidkiraly@gmail.com>
2023-11-01 20:51:45 +0100

    menu file created

---
+++ GameofLife/Menu.c
@@ -0,0 +1,14 @@
+#include "Menu.h"
+
+Gwindow Minit(){
+    Gwindow window = Gnew("Game Of Life by Zoller David",500,800);
+    Mdraw_background(window);
+    Mdrawtitle(window);
+    return window
+}
+static void Mdrawtitle(Gwindow window){
+
+}
+static void Mdraw_background(Gwindow window){
+
+}
